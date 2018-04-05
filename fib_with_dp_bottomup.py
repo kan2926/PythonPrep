ebfd3006310ebacb63330feff57e478df433d476
Kan <daivam.kanya@gmail.com>
2018-04-05 14:11:12 -0700

    Memoized fib function added

--- fib_with_dp_bottomup.py
+++ fib_with_dp_bottomup.py
@@ -8,6 +8,16 @@ def fibo(n):
         fib.append(f)
     return fib
 
+def mem_fib(n, _cache={}):
+    '''efficiently memoized recursive function, returns a Fibonacci number'''
+    if n in _cache:
+        return _cache[n]
+    elif n > 1:
+        return _cache.setdefault(n, mem_fib(n-1) + mem_fib(n-2))
+    return n
+
 if __name__ == "__main__":
     n = int(input('Enter the fibonacci nth no.:'))
-    print(fibo(n))
+    #print(fibo(n))
+    for i in range(1,n):
+        print(i,'---', mem_fib(i))
